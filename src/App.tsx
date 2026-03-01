ed03ae362c930c7c951c5626ad036f32ae09185f
Vixvic <vixvic@example.com>
2026-03-01 15:11:34 -0500

    Corregir error 404 en GitHub Pages añadiendo basename al enrutador

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bluelake-experiencia">
         <AuthProvider>
           <SeasonalProvider>
             <Routes>
